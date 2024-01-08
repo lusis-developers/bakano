5200af404e6e30fccbc62454b661ba48709d914a
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-01-08 18:49:27 -0500

    FEAT: setting component and title added

---
+++ components/BkServices.vue
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <h2 class="wrapper-title">
+      ¿Qué mismo hacemos?      
+    </h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper  {
+  background-color: $white;
+  min-height: 100vh;
+  width: 100%;
+  &-title {
+    margin: 2rem auto;
+    width: fit-content;
+    font-size: 3rem;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <div class="wrapper">
+    <div class="hero"></div>
+    <BkServices />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.wrapper {
+  .hero {
+    height: 100vh;
+    background-color: azure;
+  }
+}
+</style>
