51b7a1c5ea1ddf701f129ee1376acad5a3677803
Diego Reyes <diegorele13@gmail.com>
2024-01-20 16:55:07 -0500

    Add menuItems.ts file

---
+++ utils/menuItems.ts
@@ -0,0 +1,16 @@
+import type { Navigation } from "@/typings/LayoutTypes";
+
+export const menuItems: Navigation[] = [
+  {
+    name: 'Clientes',
+    link: '/'
+  },
+  {
+    name: 'Proyectos',
+    link: '/'
+  },
+  {
+    name: 'Contacto',
+    link: '/'
+  },
+];
