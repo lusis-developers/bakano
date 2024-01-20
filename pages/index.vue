7a05bb0e48224c424f1f0ca50eb8f47e78a590c5
Diego Reyes <diegorele13@gmail.com>
2024-01-20 16:55:45 -0500

    Add menu functionality and styling to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,24 @@
+<script setup>
+const emit = defineEmits(['toggle-menu']);
+
+const menuOpen = ref(false);
+
+function toggleMenu() {
+  menuOpen.value = !menuOpen.value;
+}
+</script>
+
 <template>
-  <div>
-    <BkHero/>
+  <div class="first-section">
+    <BkHeader @toggle-menu="toggleMenu"/>
+    <BkMenu :isVisible="menuOpen" @close-menu="toggleMenu"/>
+    <!-- <BkHero/> -->
   </div>
 </template>
+
+<style lang="scss" scoped>
+.first-section {
+  background-color: $black;
+  height: 100vh;
+}
+</style>
