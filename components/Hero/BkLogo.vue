f061b80e75c0099fe8b050b55da33686b8ec56c9
Diego Reyes <diegorele13@gmail.com>
2024-01-22 14:07:21 -0500

    Add Hero/BkLogo component with animation and styling

---
+++ components/Hero/BkLogo.vue
@@ -0,0 +1,63 @@
+<script setup>
+import gsap from 'gsap'
+import Typed from 'typed.js'
+
+onMounted(() => {
+  const logo = document.querySelector('.container-figure-image');
+  const phrase = document.querySelector('.container-figure-phrase');
+
+  gsap.from(logo, {
+    duration: 3,
+    y: '-100vh',
+    ease: 'bounce.out',
+    onComplete: () => {
+      new Typed(phrase, {
+        strings: ['Agencia digital'],
+        typeSpeed: 50,
+        startDelay: 0,
+      })
+    }
+  })
+})
+</script>
+
+<template>
+  <div class="container">
+    <figure class="container-figure">
+      <img 
+        src="/assets/images/bakano-logotipo-blanco.png" 
+        alt="Bakano" 
+        class="container-figure-image">
+      <p class="container-figure-phrase" ref="phrase">
+        
+      </p>
+    </figure>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  padding: 24px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &-figure {
+    width: 100%;
+    max-width: 800px;
+    &-image {
+      width: 100%;
+    }
+    &-phrase {
+      color: $white;
+      font-family: $primary-font;
+      font-size: 2rem; 
+      transition: color 0.5s;
+      cursor: default;
+      &:hover {
+        color: $pink;;
+      }
+    }
+  }
+}
+</style>
