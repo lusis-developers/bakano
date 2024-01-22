0c076fe310cf5fff3a279c0ca6465d89e9760a3a
Diego Reyes <diegorele13@gmail.com>
2024-01-22 14:07:32 -0500

    Add Hero component with styles and imports

---
+++ components/Hero/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+import gsap from 'gsap'
+</script>
+
+<template>
+  <div class="container">
+    <div class="container-words">
+      <BkWord />
+      <HeroBkLogo />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  background-color: $black;
+  display: flex;
+  &-words {
+    width: 100%;
+  }
+}
+</style>
