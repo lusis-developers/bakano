0fc7d931fae728a15361fbbaeb65f1cd2e299880
Diego Reyes <diegorele13@gmail.com>
2024-01-22 14:08:23 -0500

    Refactor index.vue component structure

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,26 @@ function toggleMenu() {
 </script>
 
 <template>
-  <div class="first-section">
-    <BkHeader @toggle-menu="toggleMenu"/>
-    <BkMenu :isVisible="menuOpen" @close-menu="toggleMenu"/>
-    <!-- <BkHero/> -->
+  <div class="container">
+
+    <div class="container-first-section">
+      <BkHeader @toggle-menu="toggleMenu"/>
+      <BkMenu :isVisible="menuOpen" @close-menu="toggleMenu"/>
+    </div>
+    <div class="container-second-section">
+      <Hero/>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.first-section {
-  background-color: $black;
+.container {
   height: 100vh;
+  &-first-section {
+    background-color: $black;
+  }
+  &-second-section {
+    width: 100%;
+  }
 }
 </style>
