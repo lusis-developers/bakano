030c479a8f745961f3fa05fa48d2c0912c0322ac
Jorge DC <jddelcampom@gmail.com>
2024-01-29 15:07:50 -0500

    Add emailjs/browser module and refactor form handling

---
+++ components/Form.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts">
+import CrushTextField from '@nabux-crush/crush-text-field';
+import CrushButton from '@nabux-crush/crush-button';
+import emailjs from '@emailjs/browser';
+
+const emits = defineEmits(['closeMessageFormSubmited'])
+
+const config = useRuntimeConfig();
+
+const rules = {
+  validateName: [
+    {
+      validate: (value: string) => value.length >= 6,
+      message: 'Por favor, coloca tu nombre completo'
+    }
+  ],
+  validateNumber: [
+    {
+      validate: (value: string) => /^\d+$/.test(value) && value.length > 6,
+      message: 'Por favor, ingresa tu número completo'
+    }
+  ]
+}
+
+const inputKey = ref(0)
+const form = reactive({
+  name: '',
+  phone: '',
+})
+const formIsValid = computed(() => {
+  return (
+    form.name !== '' &&
+    form.phone !== '' &&
+    rules.validateName.every(rule => rule.validate(form.name)) &&
+    rules.validateNumber.every(rule => rule.validate(form.phone))
+  )
+})
+
+function resetInputs() {
+  form.name = '';
+  form.phone = '';
+  inputKey.value++
+}
+
+async function sendEmail() {
+  const mail = {
+    from_name: form.name,
+    phoneNumber: form.phone,
+  }
+  if (formIsValid.value) {
+    try {
+      await emailjs.send(
+        config.public.serviceId,
+        config.public.templateId,
+        mail,
+        config.public.publicKey,
+      );
+      emits('closeMessageFormSubmited');
+      resetInputs();
+    } catch (error) {
+      console.error('CANNOT_SEND_EMAIL', error)
+    }
+  }
+}
+</script>
+
+<template>
+  <form class="register-wrapper-card">
+    <p class="register-wrapper-card-paragraph">
+      ¡Cada negocio es un mundo diferente! Creamos contenido y estrategias
+      BAKANES para redes sociales de acuerdo con TU NEGOCIO
+    </p>
+
+    <CrushTextField 
+      v-model="form.name" 
+      :hideLabel=true 
+      :required=true 
+      :valid-rules="rules.validateName" 
+      :key="inputKey"
+      label="Nombre de tu negocio:" 
+      placeholder="Nombre de tu negocio" />
+
+    <CrushTextField 
+      v-model="form.phone" 
+      :hideLabel=true 
+      :required=true 
+      :valid-rules="rules.validateNumber"
+      :key="inputKey" 
+      label="Número de teléfono:" 
+      placeholder="Número de teléfono" />
+
+    <div class="wrapper-button">
+      <CrushButton 
+        :small=true 
+        text="¡Arranquemos!" 
+        variant="alert" 
+        @click.prevent="sendEmail" />
+    </div>
+  </form>
+</template>
+
+<style lang="scss" scoped>
+.register-wrapper-card {
+  max-width: 80%;
+  margin: auto;
+
+  &-paragraph {
+    font-family: $secondary-font;
+    margin-bottom: .9rem;
+    font-size: $font-size-small;
+    font-weight: $font-weight-bold;
+    color: $black;
+    @media screen and (min-width: 768px) {
+      font-size: $font-size-normal;
+    }
+  }
+}
+
+.wrapper-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- pages/Home/TheHome.vue
+++ pages/Home/TheHome.vue
@@ -1,74 +1,13 @@
 <script setup lang="ts">
-import { computed, reactive, ref } from 'vue';
-import CrushTextField from '@nabux-crush/crush-text-field';
-import CrushButton from '@nabux-crush/crush-button';
-import emailjs from '@emailjs/browser';
+import { ref } from 'vue';
 
 import BakanoIsotipoNegro from "@/assets/images/bakano-isotipo-negro.png"
 import BakanoLogotipoNegro from "@/assets/images/bakano-logotipo-negro.png";
 
-const config = useRuntimeConfig();
-
-const rules = {
-  validateName: [
-    {
-      validate: (value: string) => value.length >= 6,
-      message: 'Por favor, coloca tu nombre completo'
-    }
-  ],
-  validateNumber: [
-    {
-      validate: (value: string) => /^\d+$/.test(value) && value.length > 10,
-      message: 'Por favor, ingresa tu número completo'
-    }
-  ]
-}
-
 const showMessageFormSubmited = ref(false);
-const inputKey = ref(0)
-const form = reactive({
-  name: '',
-  phone: '',
-})
-
-const formIsValid = computed(() => {
-  return (
-    form.name !== '' &&
-    form.phone !== '' &&
-    rules.validateName.every(rule => rule.validate(form.name)) &&
-    rules.validateNumber.every(rule => rule.validate(form.phone))
-  )
-})
 
 function closeMessageFormSubmited() {
   showMessageFormSubmited.value = !showMessageFormSubmited.value;
-}
-function resetInputs() {
-  form.name = '';
-  form.phone = '';
-  inputKey.value++
-}
-
-async function sendEmail() {
-  const mail = {
-    from_name: form.name,
-    phoneNumber: form.phone,
-  }
-  if (formIsValid.value) {
-    try {
-      await emailjs.send(
-        config.public.serviceId,
-        config.public.templateId,
-        mail,
-        config.public.publicKey,
-      );
-      closeMessageFormSubmited();
-      console.log('SUCCESSFULLY_SEND_EMAIL');
-      resetInputs();
-    } catch (error) {
-      console.error('CANNOT_SEND_EMAIL', error)
-    }
-  }
 }
 </script>
 
@@ -96,38 +35,7 @@ async function sendEmail() {
           alt="image" 
           class="figure-img" />
       </figure>
-      <form class="register-wrapper-card">
-        <p class="register-wrapper-card-paragraph">
-          ¡Cada negocio es un mundo diferente! Creamos contenido y estrategias
-          BAKANES para redes sociales de acuerdo con TU NEGOCIO
-        </p>
-
-        <CrushTextField 
-          v-model="form.name" 
-          :hideLabel=true 
-          :required=true 
-          :valid-rules="rules.validateName"
-          :key="inputKey" 
-          label="Nombre de tu negocio:" 
-          placeholder="Nombre de tu negocio" />
-
-        <CrushTextField 
-          v-model="form.phone" 
-          :hideLabel=true 
-          :required=true 
-          :valid-rules="rules.validateNumber"
-          :key="inputKey" 
-          label="Número de teléfono:" 
-          placeholder="Número de teléfono" />
-
-        <div class="wrapper-button">
-          <CrushButton 
-            :small=true 
-            text="¡Arranquemos!" 
-            variant="alert" 
-            @click.prevent="sendEmail" />
-        </div>
-      </form>
+      <Form @close-message-form-submited="closeMessageFormSubmited"/>
     </div>
 
     <Transition name="slide-in-down" appear>
@@ -200,22 +108,6 @@ async function sendEmail() {
 }
 
 .register-wrapper {
-  &-card {
-    max-width: 80%;
-    margin: auto;
-
-    &-paragraph {
-      font-family: $secondary-font;
-      margin-bottom: .9rem;
-      font-size: $font-size-small;
-      font-weight: $font-weight-bold;
-      color: $black;
-      @media screen and (min-width: 768px) {
-        font-size: $font-size-normal;
-      }
-    }
-  }
-
   @media screen and (min-width: 1000px){
     display: flex;
     height: max-content;
@@ -228,12 +120,6 @@ async function sendEmail() {
   }
 }
 
-.wrapper-button {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
 :deep(.crush-button) {
   border: none;
   color: $black;
@@ -244,7 +130,6 @@ async function sendEmail() {
   border-radius: 20px;
   padding: 0.7rem;
 }
-
 
 :deep(.crush-text-field .input-container .crush-text-field-input) {
   color: $black;
