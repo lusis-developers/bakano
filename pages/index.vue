4ec61716589169e00ed26494817f635c81a48a9f
Diego Reyes <diegorele13@gmail.com>
2024-01-30 13:51:34 -0500

    setting files

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <div></div>
+  <div>
+    <p>
+      hola esto es la seccion de clientes
+    </p>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+div {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
