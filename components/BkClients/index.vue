7050651b63300f35a8f65b0d93110a64e7a89791
Diego Reyes <diegorele13@gmail.com>
2024-01-30 13:51:41 -0500

    setting files

---
+++ components/BkClients/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>
+      BkClients
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  div {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    p {
+      color: black;
+    }
+  }
+</style>
---
+++ pages/webpage.vue
@@ -0,0 +1,3 @@
+<template>
+  <BkClients/>
+</template>
