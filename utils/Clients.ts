6f49f17085d004af97b9c77057ac2d3df1aab681
Diego Reyes <diegorele13@gmail.com>
2024-02-07 16:57:30 -0500

    set: clients array

---
+++ utils/Clients.ts
@@ -0,0 +1,112 @@
+import facebookLogo from '../../assets/clients/facebook.png'
+
+export const clients = [
+  {
+    backgroundColor: '#FFA07A', 
+    logo: facebookLogo, 
+    name: 'Cliente 1', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#20B2AA', 
+    logo: facebookLogo, 
+    name: 'Cliente 2', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  },
+  {
+    backgroundColor: '#778899', 
+    logo: facebookLogo, 
+    name: 'Cliente 3', 
+    clientUrl: 'https://www.google.com/' 
+  }
+];
