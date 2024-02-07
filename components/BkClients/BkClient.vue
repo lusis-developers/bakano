df8eafbbb135dd975e4f2040229e07f0115928c4
Diego Reyes <diegorele13@gmail.com>
2024-02-07 16:57:12 -0500

    add: client component

---
+++ components/BkClients/BkClient.vue
@@ -0,0 +1,48 @@
+<script setup>
+const props = defineProps({
+  backgroundColor: {
+    type: String,
+    required: true,
+  },
+  logo: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  clientUrl: {
+    type: String,
+    required: true
+  },
+})
+</script>
+
+<template>
+  <a 
+    :href="clientUrl" 
+    :style="{backgroundColor: backgroundColor}"
+    target="_blank"
+    class="square">
+    <img 
+      :src="logo" 
+      :alt="name"/>
+  </a>
+</template>
+
+<style lang="scss" scoped>
+  .square {
+    height: 256px;
+    width: 256px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    img {
+      object-fit: cover;
+      height: 50%;
+      width: 50%;
+    }
+  }
+</style>
