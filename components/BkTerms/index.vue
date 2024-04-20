656a795fe8a138300a82b248202eb09940b56005
Jorge DC <jddelcampom@gmail.com>
2024-04-19 19:48:37 -0500

    Add: PrivacyPolicy

---
+++ components/BkTerms/index.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { 
+  relevantInformation, 
+  license, 
+  unauthorizedUse, 
+  property, 
+  refundAndGuaranteePolicy, 
+  antiFraudCheck,
+  privacyPolicy 
+} from '@/utils/PrivacyPolicy';
+
+const relevantInformationArr = relevantInformation.split('\n\n');
+const relevantInformationTitle = relevantInformationArr.shift();
+const licenseArr = license.split('\n\n');
+const licenseTitle = licenseArr.shift();
+const unauthorizedUseArr = unauthorizedUse.split('\n\n');
+const unauthorizedUseTitle = unauthorizedUseArr.shift();
+const propertyArr = property.split('\n\n');
+const propertyTitle = propertyArr.shift();
+const refundAndGuaranteePolicyArr = refundAndGuaranteePolicy.split('\n\n');
+const refundAndGuaranteePolicyTitle = refundAndGuaranteePolicyArr.shift();
+const antiFraudCheckArr = antiFraudCheck.split('\n\n');
+const antiFraudCheckTitle = antiFraudCheckArr.shift();
+const privacyPolicyArr = privacyPolicy.split('\n\n');
+const privacyPolicyTitle = privacyPolicyArr.shift();
+</script>
+
+<template>
+  <div>
+    <BkTermsModel
+      :title="(relevantInformationTitle as string)"
+      :content="relevantInformationArr" />
+    <BkTermsModel
+      :title="(licenseTitle as string)"
+      :content="licenseArr" />
+    <BkTermsModel
+      :title="(unauthorizedUseTitle as string)"
+      :content="unauthorizedUseArr" />
+    <BkTermsModel
+      :title="(propertyTitle as string)"
+      :content="propertyArr" />
+    <BkTermsModel
+      :title="(refundAndGuaranteePolicyTitle as string)"
+      :content="refundAndGuaranteePolicyArr" />
+    <BkTermsModel
+      :title="(antiFraudCheckTitle as string)"
+      :content="antiFraudCheckArr" />
+    <BkTermsModel
+      :title="(privacyPolicyTitle as string)"
+      :content="privacyPolicyArr" />
+  </div>
+</template>
---
+++ pages/politica-de-privacidad.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const emit = defineEmits(['toggle-menu']);
+
+const menuOpen = ref(false);
+
+function toggleMenu():void {
+  menuOpen.value = !menuOpen.value;
+}
+</script>
+
+<template>
+  <div>
+    <LandingPageHeader 
+      :colorLogo="menuOpen" 
+      @toggle-menu="toggleMenu" />
+    <LandingPageMenu 
+      :isVisible="menuOpen" 
+      @close-menu="toggleMenu" />
+    <div class="privacy-container">
+      <h1 class="privacy-container-title">
+        Términos y Condiciones de Uso
+      </h1>
+      <BkTerms />
+    </div>
+    <LandingPageBkFooter />
+  </div>
+</template>
+
+<style lang="scss"scoped>
+:deep(.header) {
+  background-color: $black;
+}
+.privacy-container{
+  padding: 20px;
+  @media (min-width: $tablet-upper-breakpoint) {
+    max-width: 80vw;
+    margin: 0 auto;    
+  }
+  &-title{
+    font-family: $primary-font;
+    font-size: 2.5rem;
+    margin: 40px 0;
+    @media (min-width: $tablet-upper-breakpoint) {
+      font-size: 3rem;
+      text-align: center;
+    }
+  }
+}
+</style>
