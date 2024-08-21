eab03069d6a05fa23c117867b3afc36ee236da82
Diego Reyes <diegorele13@gmail.com>
2024-08-21 00:06:32 -0500

    add: styles

--- components/Main/Cta.vue
+++ components/Main/Cta.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import agencyImage from '@/assets/Cta/agencia.jpg'
-</script>
-
 <template>
   <div class="card">
     <h2 class="card-title">
@@ -17,22 +13,41 @@ import agencyImage from '@/assets/Cta/agencia.jpg'
 <style lang="scss">
 .card {
   width: 100%;
-  padding: 24px;
+  padding: 48px;
   display: flex;
   flex-direction: column;
   gap: 24px;
-  background-image: url('agencyImage');
+  background-image: url('@/assets/Cta/agencia.jpg');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+  position: relative;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(255, 255, 255, 0.2); 
+    backdrop-filter: blur(10px);
+  }
   &-title {
     font-family: $primary-font;
-    text-align: center;
+    color: $pink;
+    font-size: $font-size-extra-large;
   }
   &-subtitle {
     font-family: $primary-font;
-    text-align: center;
     font-weight: normal;
+    color: $black;
+    font-weight: bold;
+    font-size: $font-size-large;
+  }
+  &-title, &-subtitle {
+    z-index: 1; 
+    font-family: $primary-font;
+    text-align: center;
   }
 }
 </style>
