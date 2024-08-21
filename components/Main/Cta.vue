d04de474b80ceb4a653ae2d873d48fc717f9aaaa
Diego Reyes <diegorele13@gmail.com>
2024-08-20 23:43:30 -0500

    add: cta

---
+++ components/Main/Cta.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="card">
+    <h2 class="card-title">
+      Impulsa tu empresa y mejora tus servicios con Chatbots Inteligentes
+    </h2>
+    <h3 class="card-subtitle">
+      Creamos conversaciones personalizadas, atractivas e inteligentes, para mejorar la experiencia de tu cliente, potenciando la conversión y preferencia.
+    </h3>
+    <GlobalBkButton/>
+  </div>
+</template>
+
+<style lang="scss">
+.card {
+  width: 100%;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  &-title {
+    font-family: $primary-font;
+    text-align: center;
+  }
+  &-subtitle {
+    font-family: $primary-font;
+    text-align: center;
+  }
+}
+</style>
