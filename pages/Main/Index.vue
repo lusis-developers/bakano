55603bf79d81a5f32588e86bb0a14fb22bd07acd
Diego Reyes <diegorele13@gmail.com>
2024-08-20 23:11:12 -0500

    add: experience component to main

--- pages/Main/Index.vue
+++ pages/Main/Index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Hero from '@/components/Main/Hero.vue';
 import Results from '@/components/Main/Results.vue';
+import Experience from '@/components/Main/Experience.vue';
 </script>
 
 <template>
   <div class="main">
     <Hero />
     <Results />
+    <Experience />
   </div>
 </template>
 
