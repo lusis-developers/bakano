c88190f82749c7c52a333bd78aa4ea028f90e738
Diego Reyes <diegorele13@gmail.com>
2024-08-21 00:11:45 -0500

    done cta

--- components/Main/Cta.vue
+++ components/Main/Cta.vue
@@ -29,7 +29,7 @@
     left: 0;
     right: 0;
     bottom: 0;
-    background-color: rgba(255, 255, 255, 0.2); 
+    background-color: rgba(255, 244, 244, 0.2); 
     backdrop-filter: blur(10px);
   }
   &-title {
