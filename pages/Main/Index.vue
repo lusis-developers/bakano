df9fe6d783a27ce80fb6a5078f5784b9292810d5
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-08-21 01:26:44 -0500

    ADD: customer tarket

--- pages/Main/Index.vue
+++ pages/Main/Index.vue
@@ -2,12 +2,14 @@
 import Hero from '@/components/Main/Hero.vue';
 import Results from '@/components/Main/Results.vue';
 import Experience from '@/components/Main/Experience.vue';
+import CustomerTarget from '~/components/Main/CustomerTarget.vue';
 </script>
 
 <template>
   <div class="main">
     <Hero />
     <Results />
+    <CustomerTarget />
     <Experience />
   </div>
 </template>
