f282948a884294bd51d15625e5c46adc424871d0
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-08-22 15:42:01 -0500

    FEAT: added layout globally, ADD: portfolio page

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import BkHeader from '@/components/BkHeader.vue';
+import BkFooter from '@/components/BkFooter.vue';
+</script>
+
+<template>
+  <div>
+    <BkHeader />
+    <slot />
+    <BkFooter />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
-import Main from '@/pages/Main/Index.vue';
-import BkHeader from '@/components/BkHeader.vue';
-import BkFooter from '@/components/BkFooter.vue';
+import Cta from '@/components/Main/Cta.vue';
+import Hero from '@/components/Main/Hero.vue';
+import Results from '@/components/Main/Results.vue';
+import Services from '@/components/Main/Services.vue';
+import Experience from '@/components/Main/Experience.vue';
+import CustomerTarget from '~/components/Main/CustomerTarget.vue';
 </script>
 
 <template>
-  <div>
-    <BkHeader />
-    <Main />
-    <BkFooter />
+  <div class="main">
+    <Hero />
+    <Results />
+    <CustomerTarget />
+    <Experience />
+    <Cta />
+    <Services />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.main {
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
