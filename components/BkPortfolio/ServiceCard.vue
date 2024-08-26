543aa64d7ee374e209225e85d7977b47a82b3e0a
Diego Reyes <diegorele13@gmail.com>
2024-08-26 18:06:46 -0500

    add: bkportofolio

---
+++ components/BkPortfolio/ServiceCard.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+
+interface Detail {
+  title: string;
+  detail: string;
+}
+
+const props = defineProps({
+  projectType: {
+    type: String as PropType<string>,
+    required: true,
+    default: 'CHATBOT'
+  },
+  projectTitle: {
+    type: String as PropType<string>,
+    required: true,
+  },
+  projectDescription: {
+    type: String as PropType<string>,
+    required: true,
+  },
+  projectSubTitle: {
+    type: String as PropType<string>,
+    required: true,
+    default: 'Detalles del proyecto:'
+  },
+  details: {
+    type: Array as PropType<Detail[]>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="card-project">
+    <GlobalBkCard>
+      <template #title>
+        <h5 class="card-project-type">
+          {{ projectType }}
+        </h5>
+        <h2 class="card-project-title">
+          {{ projectTitle }}
+        </h2>
+      </template>
+      <template #content>
+        <h4 class="card-project-description">
+          {{ projectDescription }}
+        </h4>
+        <h3 class="card-project-sub">
+          {{ projectSubTitle }}
+        </h3>
+        <ul 
+          v-for="(detail, index) in props.details"
+          :key="index"
+          class="card-project-list">
+          <li class="card-project-list-item">
+            <span class="card-project-list-item-title">
+              {{ detail.title }}:
+            </span>
+            <span class="card-project-list-item-detail">
+              {{ detail.detail }}
+            </span>
+          </li>
+        </ul>
+      </template>
+    </GlobalBkCard>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card-project {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  &-type {
+    color: $pink;
+    font-family: $primary-font;
+    padding-bottom: 16px;
+  }
+  &-title {
+    color: $black;
+    font-family: $primary-font;
+    padding-bottom: 16px;
+  }
+  &-description {
+    color: $black;
+    font-family: $primary-font;
+    font-weight: 500;
+  }
+  &-sub {
+    padding-top: 24px;
+    color: $black;
+    font-family: $primary-font;
+    font-size: $font-size-large;
+  }
+  &-list {
+    color: $black;
+    font-family: $primary-font;
+    &-item {
+      color: $black;
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      padding: 16px 0;
+      border-bottom: 1px solid $purple;
+      &-title {
+        font-weight: bold;
+        font-size: $font-size-normal;
+      }
+    }
+  }
+}
+</style>
---
+++ components/BkPortfolio/Services.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import useServiceStore from '~/stores/ServiceStore';
+
+const services = useServiceStore();
+</script>
+
+<template>
+  <div class="container crush-container crush-two-column-layout" v-for="(service, index) in services.services" :key="index">
+    <figure class="container-video">
+      <img 
+        :src="service.projectImage" 
+        alt="primer servicio"
+        class="container-video-img" >
+    </figure>
+    <div class="container-content">
+      <BkPortfolioServiceCard
+        :projectType="service.projectType"
+        :projectTitle="service.projectTitle"
+        :projectDescription="service.projectDescription"
+        :projectSubTitle="service.projectSubTitle"
+        :details="service.details" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  padding: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+  &-video {
+    width: 100%;
+    height: 600px;
+    @media (min-width: $tablet-lower-breakpoint) {
+      max-width: 600px;
+      max-height: 600px;
+    }
+    &-img {
+      width: 100%;
+      height: 100%;
+      border-radius: 8px;
+      object-fit: cover;
+    }
+  }
+  &-content {
+    max-width: 500px;
+  }
+}
+</style>
